ddc57034cc71a017c21d284bd53cccdf9ad656d4
Benjamin Mazenko <dbds@iup.edu>
2016-04-06 14:07:39 -0400

    Added a divider

--- app/src/main/java/com/example/bmaz/coscproject/ThirdFragment.java
+++ app/src/main/java/com/example/bmaz/coscproject/ThirdFragment.java
@@ -1,17 +1,18 @@
 package com.example.bmaz.coscproject;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TabWidget;
 import android.widget.TextView;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -39,8 +40,6 @@ public class ThirdFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // thirdView = inflater.inflate(R.layout.fragment_third, container, false);
-
         // Create the TabWidget (the tabs)
         TabWidget tabWidget = new TabWidget(getContext());
         tabWidget.setId(android.R.id.tabs);
@@ -48,7 +47,7 @@ public class ThirdFragment extends Fragment {
         FrameLayout frame = new FrameLayout(getContext());
         frame.setId(android.R.id.tabcontent);
         LinearLayout.LayoutParams frameLayoutParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1);
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1);
         frameLayoutParams.setMargins(1, 1, 1, 1);
         frame.setLayoutParams(frameLayoutParams);
         // Create the container for the above widgets
@@ -58,13 +57,22 @@ public class ThirdFragment extends Fragment {
         tabHostLayout.addView(frame);
         // Create FragmentTabHost
         mTabHost = new FragmentTabHost(getActivity());
-        // mTabHost.addView(tabHostLayout);
         tabHostLayout.addView(mTabHost);
         mTabHost.setup(getActivity(), getChildFragmentManager(), android.R.id.tabcontent);
+
         // add the tabs
+        mTabHost.getTabWidget().setShowDividers(TabWidget.SHOW_DIVIDER_MIDDLE);
+        mTabHost.getTabWidget().setDividerDrawable( R.drawable.divider_vertical_dark );
         mTabHost.addTab(mTabHost.newTabSpec("students").setIndicator("Student Speakers"), StudentPresentersTab.class, null);
         mTabHost.addTab(mTabHost.newTabSpec("keynotes").setIndicator("Keynote Speakers"), KeyNotePresentersTab.class, null);
         mTabHost.addTab(mTabHost.newTabSpec("faculty").setIndicator("Faculty Speakers"), FacultyPresentersTab.class, null);
+
+        for(int i=0;i<mTabHost.getTabWidget().getChildCount();i++)
+        {
+            mTabHost.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#7B241C"));
+        }
+
+
         return tabHostLayout;
 
     }
