6b97934cff4bf1e1b6d1f18c5270448300d726df
DJoliet <danpjol@gmail.com>
2016-04-26 19:08:24 -0400

    Update ScheduleSaturdayTab.java
    Added the additional eleven strings.

--- app/src/main/java/com/example/bmaz/coscproject/ScheduleSaturdayTab.java
+++ app/src/main/java/com/example/bmaz/coscproject/ScheduleSaturdayTab.java
@@ -21,7 +21,18 @@ public class ScheduleSaturdayTab extends ParentFragmentLayout {
         presentorsInfo = new String[][]{
                 {getResources().getString(R.string.satOne)},
                 {getResources().getString(R.string.satTwo)},
-                {getResources().getString(R.string.satThree)}
+                {getResources().getString(R.string.satThree)},
+                {getResources().getString(R.string.satFour)},
+                {getResources().getString(R.string.satFive)},
+                {getResources().getString(R.string.satSix)},
+                {getResources().getString(R.string.satSeven)},
+                {getResources().getString(R.string.satEight)},
+                {getResources().getString(R.string.satNine)},
+                {getResources().getString(R.string.satTen)},
+                {getResources().getString(R.string.satEleven)},
+                {getResources().getString(R.string.satTwelve)},
+                {getResources().getString(R.string.satThirteen)},
+                {getResources().getString(R.string.satFourteen)}
         };
     }
 
